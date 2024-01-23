3985dd44a1deabf9a01919b51054f13e316a6bb6
Daria Ziobrowska <darianocon@gmail.com>
2024-01-23 20:52:12 +0100

    [chore] Integration test - adding cards

--- src/components/Board/__tests__/integration/Board.container.test.tsx
+++ src/components/Board/__tests__/integration/Board.container.test.tsx
@@ -1,3 +1,42 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import BoardContainer from '../../Board.container'
+import { describe, expect, it, vi } from 'vitest'
+import * as dataModule from '../../../../data'
+
 describe('BoardContainer integration tests', () => {
-  // TODO: Add unit tests
+  // MSW version
+  it('adds a new card when <CardAddNew /> is clicked', async () => {
+    // Render the BoardContainer
+    render(<BoardContainer />)
+
+    // Simulate click on <CardAddNew />
+    await waitFor(() => {
+      fireEvent.click(screen.getByRole('button', { name: 'Add new card' }))
+
+      // Check if a new card is displayed
+      expect(screen.getByText('Click to start noting')).toBeInTheDocument()
+    })
+  })
+
+  // Mock version
+  it('adds a new card when <CardAddNew /> is clicked', async () => {
+    // Mock the findCards function and POST request in CardModel
+    vi.spyOn(dataModule, 'findCards').mockResolvedValue([])
+    const createCardSpy = vi.
+      spyOn(dataModule.CardModel.prototype, 'create')
+      .mockResolvedValue()
+
+    // Render the BoardContainer
+    render(<BoardContainer />)
+
+    // Simulate click on <CardAddNew />
+    await waitFor(() => {
+      fireEvent.click(screen.getByRole('button', {name: 'Add new card' }))
+
+      // Check if a new card is displayed
+      expect(screen.getByText('Click to start noting')).toBeInTheDocument()
+    })
+
+    expect(createCardSpy).toHaveBeenCalledTimes(1)
+  })
 })
